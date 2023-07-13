4a98568569e1607320e4625461e46a5a9680e49e
Yohannes <ydegu@ADMIN.CROSSROADS.CO>
2023-07-13 12:44:30 +0300

    github and google login

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,10 +2,10 @@ import Input from "@/components/Input";
 import axios from "axios";
 import { useCallback, useState } from "react";
 import { signIn } from "next-auth/react";
-import { useRouter } from 'next/router';
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 const Auth = () => {
-  const router = useRouter();
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
@@ -20,14 +20,12 @@ const Auth = () => {
       await signIn("credentials", {
         email,
         password,
-        redirect: false,
-        callbackUrl: "/",
+        callbackUrl: "/profiles",
       });
-      router.push("/");
     } catch (error) {
       console.group(error);
     }
-  }, [email, password, router]);
+  }, [email, password]);
 
   //register
   const register = useCallback(async () => {
@@ -86,6 +84,20 @@ const Auth = () => {
             >
               {variant == "login" ? "Login" : "Sign Up"}
             </button>
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div
+                onClick={() => signIn("google", { callbackUrl: "/profiles" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FcGoogle size={30} />
+              </div>
+              <div
+                onClick={() => signIn("github", { callbackUrl: "/profiles" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FaGithub size={30} />
+              </div>
+            </div>
             <p className="text-neutral-500 mt-12">
               {variant === "login"
                 ? "First time using Netflix?"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,33 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+}
 
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
   return (
     <>
-    <h1 className="text-red-600">Netflix Clone</h1>
+      <h1 className="text-4xl text-green-500">Netflix Clone</h1>
+      <p className="text-white">Logged in as : {user?.name}</p>
+      <button onClick={() => signOut()} className="h-10 w-full bg-white">
+        Logout
+      </button>
     </>
-  )
+  );
 }
